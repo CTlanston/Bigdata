8ed5ce27444c4ea828e0e4b0ad7d554ac83232b6
Lanston (Tao) Chen <115934184+CTlanston@users.noreply.github.com>
2024-01-14 21:01:20 -0500

    Add files via upload

---
+++ AWS/MapReduce & HDFS/word_count.py
@@ -0,0 +1,40 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+
+class MRMostUsedWord(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_words,
+                   combiner=self.combiner_count_words,
+                   reducer=self.reducer_count_words),
+            MRStep(reducer=self.reducer_find_max_word)
+        ]
+
+    def mapper_get_words(self, _, line):
+        clean_line = re.sub(r'[^a-zA-Z0-9\s]', '', line)
+        for word in WORD_RE.findall(clean_line):
+            yield (word.lower(), 1)
+
+    def combiner_count_words(self, word, counts):
+        # optimization: sum the words we've seen so far
+        yield (word, sum(counts))
+
+    def reducer_count_words(self, word, counts):
+        # send all (num_occurrences, word) pairs to the same reducer.
+        # num_occurrences is so we can easily use Python's max() function.
+        yield None, (sum(counts), word)
+
+    # discard the key; it is just None
+    def reducer_find_max_word(self, _, word_count_pairs):
+        max_pair = max(word_count_pairs)
+        yield ("Most Frequent Word", max_pair[1])
+        yield ("Frequency", max_pair[0])
+
+
+if __name__ == '__main__':
+    MRMostUsedWord.run()
