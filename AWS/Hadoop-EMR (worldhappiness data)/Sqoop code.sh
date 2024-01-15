5c957a8a184689702946236502b3b95b012624d7
Lanston (Tao) Chen <115934184+CTlanston@users.noreply.github.com>
2024-01-14 21:22:05 -0500

    Update Sqoop code.sh

--- AWS/Hadoop-EMR (worldhappiness data)/Sqoop code.sh
+++ AWS/Hadoop-EMR (worldhappiness data)/Sqoop code.sh
@@ -1,1 +1,9 @@
-
+sqoop import \
+--connect
+jdbc:mysql://database-lab5.c3f4g2yxdyve.us-east-1.rds.amazonaws.com/happy
+\
+--username admin \
+--password Ct123456 \
+--table happy2018 \
+--target-dir /user/hadoop/lab5 \
+--split-by id
